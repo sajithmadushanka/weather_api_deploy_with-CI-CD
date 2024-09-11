a2821a75569f61f5bfb7fa1ae40d430db1f9655c
sajeebro <sajithmadusanka720@gmail.com>
2024-09-12 03:33:05 +0530

    changed port 3000 to 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Purpose: Entry point for the server
 const app = require('./index');
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
